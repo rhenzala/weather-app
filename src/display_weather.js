faf4db2b889f107261216e7271777454316ece3f
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-04 22:48:22 +0800

    Add icon handler and date formatter
    Also, modified the createWeatherDetailGrid to add symbols to value for
    better ux.

--- src/display_weather.js
+++ src/display_weather.js
@@ -1,12 +1,17 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { format } from 'date-fns';
 import getWeather from './fetch_data';
 import cloudy from '../assets/cloud.svg';
 import sunny from '../assets/sun.svg';
+import rain from '../assets/rain.svg';
+import thunder from '../assets/thunder.svg';
+import wind from '../assets/wind.svg';
+import snow from '../assets/snow.svg'
 
 const HandleWeatherDisplay = (() => {
     const content = document.getElementById('content');
-    let weatherImage = sunny;
 
-    const createWeatherDetailGrid = (weatherDetailsList, weatherDetails) => {
+    const createWeatherDetailGrid = (weatherDetailsList, weatherDetails, tempUnit) => {
         Object.entries(weatherDetailsList).forEach(([key, value]) => {
             const name = document.createElement('p');
             const data = document.createElement('p');
@@ -14,14 +19,32 @@ const HandleWeatherDisplay = (() => {
             data.classList.add('value');
             const gridItem = document.createElement('div');
             gridItem.className = 'grid-item';
-            data.textContent = value;
+            if (key === 'Max' || key === 'Min' || key === 'Feels Like') {
+                data.textContent = `${value}°`;
+            } else if (key === 'Windspeed') {
+                if (tempUnit === 'celsius') {
+                    data.textContent = `${value} kph`;
+                } else {
+                    data.textContent = `${value} mph`;
+                }
+            } else if (key === 'Visibility') {
+                if (tempUnit === 'celsius') {
+                    data.textContent = `${value} km`;
+                } else {
+                    data.textContent = `${value} miles`;
+                }
+            } else if (key === 'Humidity') {
+                data.textContent = `${value} %`;
+            } else {
+                data.textContent = value;
+            }
             name.textContent = key;
             gridItem.appendChild(data)
             gridItem.appendChild(name)
             weatherDetails.appendChild(gridItem);
         })
     }
-    const createCard = (image, cityName, date, temperature, condition, weatherDetailsList) => {
+    const createCard = (image, cityName, date, temperature, condition, weatherDetailsList, tempUnit) => {
         const weatherCard = document.createElement('div');
         const generalDetails = document.createElement('div');
         const weatherTemp = document.createElement('div');
@@ -49,8 +72,7 @@ const HandleWeatherDisplay = (() => {
         dateToday.textContent = date;
         temp.textContent = `${temperature}°`;
         cond.textContent = condition;
-        createWeatherDetailGrid(weatherDetailsList, weatherDetails);
-
+        createWeatherDetailGrid(weatherDetailsList, weatherDetails, tempUnit);
 
         generalDetails.appendChild(location)
         generalDetails.appendChild(dateToday)
@@ -62,12 +84,33 @@ const HandleWeatherDisplay = (() => {
         weatherCard.appendChild(weatherDetails)
         content.appendChild(weatherCard)
     }
-    
-    const displayWeather = (weatherData) => {
+    const handleWeatherIcon = (weatherIcon) => {
+        let weatherImg = sunny;
+        if (weatherIcon.includes('cloudy')) {
+            weatherImg = cloudy
+        } else if (weatherIcon.includes('rain')){
+            weatherImg = rain
+        } else if (weatherIcon.includes('thunder')) {
+            weatherImg = thunder
+        } else if (weatherIcon.includes('wind')) {
+            weatherImg = wind
+        } else if (weatherIcon.includes('snow')) {
+            weatherImg = snow
+        } else if (weatherIcon.includes('clear')) {
+            weatherImg = sunny
+        }
+        return weatherImg;
+    }
+    const formatDate = (date) => {
+        const newDate = new Date(date);
+        return format(newDate, 'MMMM d, yyyy')
+    }
+    const displayWeather = (weatherData, tempUnit) => {
         content.replaceChildren();
         const cityName = weatherData.resolvedAddress;
         const date = weatherData.days[0].datetime;
         const condition = weatherData.days[0].conditions;
+        const weatherIcon = weatherData.days[0].icon;
         const temperature = weatherData.days[0].temp;
         const maxTemp = weatherData.days[0].tempmax;
         const minTemp = weatherData.days[0].tempmin;
@@ -78,8 +121,8 @@ const HandleWeatherDisplay = (() => {
         const citySunrise = weatherData.days[0].sunrise;
         const citySunset = weatherData.days[0].sunset;
         const weatherDetailsList = {
-            'Max Temperature':maxTemp, 
-            'Min Temperature':minTemp, 
+            'Max':maxTemp, 
+            'Min':minTemp, 
             'Feels Like':feelsLike, 
             'Humidity':cityHumidity, 
             'Visibility':cityVisibility, 
@@ -87,14 +130,13 @@ const HandleWeatherDisplay = (() => {
             'Sunrise':citySunrise, 
             'Sunset':citySunset
         };
-        if (condition === 'Cloudy') {
-            weatherImage = cloudy
-        }
-        createCard(weatherImage, cityName, date, temperature, condition, weatherDetailsList);
+        const weatherImage = handleWeatherIcon(weatherIcon);
+        const formatedDate = formatDate(date);
+        createCard(weatherImage, cityName, formatedDate, temperature, condition, weatherDetailsList, tempUnit);
     }
     const init = async (city, tempUnit) => {
         const weatherData = await getWeather(city, tempUnit);
-        displayWeather(weatherData);
+        displayWeather(weatherData, tempUnit);
     }
     return {
         init
