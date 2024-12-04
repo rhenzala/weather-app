32a151cb618b28a6945e50c78db474f5ce30f13e
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-04 17:23:19 +0800

    Add error handling when fetching data

--- src/fetch_data.js
+++ src/fetch_data.js
@@ -1,34 +1,20 @@
+
 async function getWeather (city, tempUnit) {
-    let response;
-    if (tempUnit === 'celsius') {
-        response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=YY3ZSPFBRS3934E42YAVMXKTH
-    `, {mode: 'cors'});
-    } else if (tempUnit === 'fahrenheit') {
-        response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=YY3ZSPFBRS3934E42YAVMXKTH
-    `, {mode: 'cors'});
+    const API_KEY = 'YY3ZSPFBRS3934E42YAVMXKTH';
+    try{
+        const unitGroup = tempUnit === 'celsius' ? 'metric': 'us';
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unitGroup}&key=${API_KEY}
+        `, {mode: 'cors'});
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const weatherData = await response.json();
+        return weatherData
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        throw error; 
     }
-    const weatherData = await response.json();
-    console.log(weatherData)
-    const cityName = weatherData.resolvedAddress;
-    const weatherDesc = weatherData.description;
-    const condition = weatherData.days[0].conditions;
-    const feelsLike = weatherData.days[0].feelslike;
-    const temperature = weatherData.days[0].temp;
-    const cityHumidity = weatherData.days[0].humidity;
-    const cityVisibility = weatherData.days[0].visibility;
-    const cityWindspeed = weatherData.days[0].windspeed;
-    const citySunrise = weatherData.days[0].sunrise;
-    const citySunset = weatherData.days[0].sunset;
-    console.log('city:', cityName)
-    console.log('description:', weatherDesc)
-    console.log('condition:', condition)
-    console.log('feels like:', feelsLike)
-    console.log('temp:', temperature)
-    console.log('humidity:', cityHumidity)
-    console.log('visibility:', cityVisibility)
-    console.log('windspeed:', cityWindspeed)
-    console.log('sunrise:', citySunrise)
-    console.log('sunset:', citySunset)
+    
 }
 
 export default getWeather
